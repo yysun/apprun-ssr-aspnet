fd41ff3b61a8836ffda4ad176fd3f1a54316f30b
Yiyi Sun <yiyisun@gmail.com>
2017-09-14 17:14:48 -0400

    initialize project

---
+++ WebApplication2/VirtualDomAttribute.cs
@@ -0,0 +1,36 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication2
+{
+    public class VirtualDomAttribute : FilterAttribute, IResultFilter
+    {
+        StringWriter textWriter;
+        TextWriter originalWriter; 
+        public void OnResultExecuting(ResultExecutingContext filterContext)
+        {
+            originalWriter = filterContext.HttpContext.Response.Output;
+            textWriter = new StringWriter(CultureInfo.InvariantCulture);
+            filterContext.HttpContext.Response.Output = textWriter;
+        }
+
+        public void OnResultExecuted(ResultExecutedContext filterContext)
+        {
+            var capturedText = textWriter.ToString();
+
+            var doc = new HtmlDocument();
+            doc.LoadHtml(capturedText);
+
+            filterContext.HttpContext.Response.Output = originalWriter;
+            filterContext.HttpContext.Response.Write(capturedText);
+        }
+
+    }
+}
